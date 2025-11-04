9577e465dcdb2767722d149618e83a4eadf6d907
anildalar <dollorinfotech@gmail.com>
2025-11-04 09:59:33 +0530

    new

---
+++ 4Nov2025/prisma/migrations/20251104040918_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Students` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fname` VARCHAR(191) NOT NULL,
+    `lname` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
