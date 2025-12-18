8dbae13b7257d89c039a6a1bb7cc56c3023a4691
anildalar <dollorinfotech@gmail.com>
2025-12-18 10:01:39 +0530

    new

--- 16Dec2025/index2.js
+++ 16Dec2025/index2.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// ✅ Enable JSON body parsing
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -9,6 +12,20 @@ app.get('/', (req, res) => {
 app.get('/aboutus', (req, res) => {
   res.send('About Us Page!')
 })
+
+//object.method(aa1,aa2);
+app.post('/api/auth/local/register',(req,res)=>{
+  //req.query
+  console.log('req.query >>>.',req.query);
+  //req.params
+  console.log('req.params >>>.',req.params);
+  console.log('req.body >>>.',req.body);
+
+  console.log(req.body.fname);
+  
+  res.status(200).send('Hello From Register Route');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
